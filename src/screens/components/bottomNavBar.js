2636503d27eea6f16ad39408e7218f16444eb32e
Bilal Qadar <qadarbilal@gmail.com>
2020-10-15 22:30:59 -0400

    Updated bottom bar

--- src/screens/components/bottomNavBar.js
+++ src/screens/components/bottomNavBar.js
@@ -47,20 +47,20 @@ export default class BottomNavBar extends Component {
       <View style={styles.container}>
         <View style={styles.footer}>
           <TouchableOpacity onPress={() => this.goToChatScreen()}>
-            <View style={navigation.state.routeName === "Dialogs" ? [styles.footerChosen, {marginLeft: 20}]: [styles.footerElement, {marginLeft: 20}]}>
-              <Icon name="chatbubbles" size={35} color={navigation.state.routeName === "Dialogs" ? "#2E86FB": "black"}/>
+            <View style={navigation.state.routeName === "Dialogs" ? [styles.footerChosen, {marginLeft: 30}]: [styles.footerElement, {marginLeft: 30}]}>
+              <Icon name="chatbox-outline" size={40} color={navigation.state.routeName === "Dialogs" ? "#2E86FB": "black"}/>
             </View>
           </TouchableOpacity>
 
           <TouchableOpacity onPress={() => this.goToPeopleScreen()}>
             <View style={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? [styles.footerChosen]: styles.footerElement}>
-              <Icon name="people-outline" size={35} color={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? "#2E86FB" : "black"}/>
+              <Icon name="people-outline" size={40} color={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? "#2E86FB" : "black"}/>
             </View>
           </TouchableOpacity>
 
           <TouchableOpacity onPress={() => this.goToEventsScreen()}>
-            <View style={navigation.state.routeName === "Events" ? [styles.footerChosen, {marginRight: 20}]: [styles.footerElement, {marginRight: 20}]}>
-              <Icon name="settings-outline" size={35} color={navigation.state.routeName === "Events" ? "#2E86FB": "black"}/>
+            <View style={navigation.state.routeName === "Events" ? [styles.footerChosen, {marginRight: 30}]: [styles.footerElement, {marginRight: 30}]}>
+              <Icon name="flag-outline" size={40} color={navigation.state.routeName === "Events" ? "#2E86FB": "black"}/>
             </View>
           </TouchableOpacity>
         </View>
@@ -72,7 +72,7 @@ export default class BottomNavBar extends Component {
 const styles = StyleSheet.create({
   container: {
     position: 'absolute',
-    bottom: SIZE_SCREEN.height/11,
+    bottom: SIZE_SCREEN.height/12,
     marginLeft: (SIZE_SCREEN.width/2) - (SIZE_SCREEN.width * 0.9/2),
   },
   footer: {
@@ -100,19 +100,19 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     justifyContent: "center",
     alignItems: 'center',
-    paddingBottom: 15,
-    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
-    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
+    paddingBottom: 25,
+    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 20,
+    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 20,
   },
   footerChosen: {
-    height: SIZE_SCREEN.height/8.5,
+    height: SIZE_SCREEN.height/8,
     backgroundColor: 'white',
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
-    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
-    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
+    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 20,
+    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 20,
     paddingTop: 0,
-    paddingBottom: 15,
+    paddingBottom: 25,
     justifyContent: "center",
     alignItems: 'center',
     borderLeftWidth: 1,
