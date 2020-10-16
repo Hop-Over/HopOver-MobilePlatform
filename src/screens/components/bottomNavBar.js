e78bf92d38af78463eb7d0c651eed7601a54d7f2
Bilal Qadar <qadarbilal@gmail.com>
2020-10-15 21:01:49 -0400

    Updated bottom nav bar

--- src/screens/components/bottomNavBar.js
+++ src/screens/components/bottomNavBar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { StyleSheet, View, FlatList, Text, StatusBar, TouchableOpacity, Platform, ScrollView } from 'react-native'
 import { StackActions, NavigationActions } from 'react-navigation';
 import { SIZE_SCREEN } from '../../helpers/constants'
-import Icon from 'react-native-vector-icons/Feather'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class BottomNavBar extends Component {
 
@@ -46,21 +46,21 @@ export default class BottomNavBar extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.footer}>
-          <TouchableOpacity onPress={() => this.goToPeopleScreen()}>
-            <View style={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? styles.footerChosenLeft: styles.footerElement}>
-              <Icon name="users" size={35} color={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? "#2E86FB" : "black"}/>
+          <TouchableOpacity onPress={() => this.goToChatScreen()}>
+            <View style={navigation.state.routeName === "Dialogs" ? [styles.footerChosen, {marginLeft: 20}]: [styles.footerElement, {marginLeft: 20}]}>
+              <Icon name="chatbubbles" size={35} color={navigation.state.routeName === "Dialogs" ? "#2E86FB": "black"}/>
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => this.goToChatScreen()}>
-            <View style={navigation.state.routeName === "Dialogs" ? styles.footerChosenCenter: styles.footerElement}>
-              <Icon name="message-square" size={35} color={navigation.state.routeName === "Dialogs" ? "#2E86FB": "black"}/>
+          <TouchableOpacity onPress={() => this.goToPeopleScreen()}>
+            <View style={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? [styles.footerChosen]: styles.footerElement}>
+              <Icon name="people-outline" size={35} color={navigation.state.routeName === "People" || navigation.state.routeName === "Search" || navigation.state.routeName === "Requests" ? "#2E86FB" : "black"}/>
             </View>
           </TouchableOpacity>
 
           <TouchableOpacity onPress={() => this.goToEventsScreen()}>
-            <View style={navigation.state.routeName === "Events" ? styles.footerChosenRight: styles.footerElement}>
-              <Icon name="flag" size={35} color={navigation.state.routeName === "Events" ? "#2E86FB": "black"}/>
+            <View style={navigation.state.routeName === "Events" ? [styles.footerChosen, {marginRight: 20}]: [styles.footerElement, {marginRight: 20}]}>
+              <Icon name="settings-outline" size={35} color={navigation.state.routeName === "Events" ? "#2E86FB": "black"}/>
             </View>
           </TouchableOpacity>
         </View>
@@ -72,9 +72,8 @@ export default class BottomNavBar extends Component {
 const styles = StyleSheet.create({
   container: {
     position: 'absolute',
-    bottom: SIZE_SCREEN.height/10,
+    bottom: SIZE_SCREEN.height/11,
     marginLeft: (SIZE_SCREEN.width/2) - (SIZE_SCREEN.width * 0.9/2),
-    backgroundColor: 'red'
   },
   footer: {
     flexDirection: 'row',
@@ -82,16 +81,18 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignItems: 'center',
     justifyContent: 'space-between',
-    borderTopWidth: 1,
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderTopColor: '#D1D1D1',
-    borderLeftColor: '#D1D1D1',
-    borderRightColor: '#D1D1D1',
     backgroundColor: 'white',
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
     height: SIZE_SCREEN.height/10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   footerElement:{
     paddingLeft: 40,
@@ -99,58 +100,25 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     justifyContent: "center",
     alignItems: 'center',
+    paddingBottom: 15,
+    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
+    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
   },
-  footerText:{
-    color: 'grey',
-    fontSize: 16,
-  },
-  footerTextCurrent:{
-    color: 'black',
-    fontSize: 16,
-  },
-  footerChosenLeft: {
-    height: SIZE_SCREEN.height/8,
+  footerChosen: {
+    height: SIZE_SCREEN.height/8.5,
     backgroundColor: 'white',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    marginLeft: 20,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
     paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
     paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
     paddingTop: 0,
+    paddingBottom: 15,
     justifyContent: "center",
     alignItems: 'center',
     borderLeftWidth: 1,
     borderRightWidth: 1,
     borderLeftColor: '#00000014',
     borderRightColor: '#00000014',
-    borderTopWidth: 8,
-    borderTopColor: "#2E86FB",
-    shadowColor: "#000",
-    shadowOffset: {
-    	width: 0,
-    	height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  footerChosenRight: {
-    height: SIZE_SCREEN.height/8,
-    backgroundColor: 'white',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    marginRight: 20,
-    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) -10,
-    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
-    paddingTop: 0,
-    justifyContent: "center",
-    alignItems: 'center',
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderLeftColor: '#00000014',
-    borderRightColor: '#00000014',
-    borderTopWidth: 8,
-    borderTopColor: "#2E86FB",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -160,29 +128,4 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
-  footerChosenCenter: {
-    height: SIZE_SCREEN.height/8,
-    backgroundColor: 'white',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    paddingLeft: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
-    paddingRight: SIZE_SCREEN.width - (SIZE_SCREEN.width * 0.9) - 10,
-    paddingTop: 0,
-    justifyContent: "center",
-    alignItems: 'center',
-    borderLeftWidth: 1,
-    borderRightWidth: 1,
-    borderLeftColor: '#00000014',
-    borderRightColor: '#00000014',
-    borderTopWidth: 8,
-    borderTopColor: "#2E86FB",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  }
 })
